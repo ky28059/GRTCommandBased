966fc72311836be05ecf6cd55c7a9902fb926710
Ethan <e3lcool@gmail.com>
2021-10-15 14:35:20 -0700

    Parallel solenoids implementation

---
+++ src/main/java/frc/robot/subsystems/solenoids/ParallelSolenoids.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.solenoids;
+
+public class ParallelSolenoids {
+    SolenoidGroup[] solenoids;
+
+    public ParallelSolenoids(SolenoidGroup... solenoids) {
+        this.solenoids = solenoids;
+    }
+
+    public void set(boolean extended) {
+        for (SolenoidGroup solenoid : solenoids) {
+            solenoid.set(extended);
+        }
+    }
+}
